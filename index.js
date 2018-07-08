8033ed8d61cd33e65ef21175cf08736c3be88b92
Jasper John Cinatad <slytherin9090@gmail.com>
2018-07-08 17:55:07 +0800

    Added grades endpoint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const { login, students } = require('./routes'); 
+const { login, students, grades } = require('./routes'); 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 //routes
 app.use(login);
 app.use(students);
+app.use(grades);
 
 app.listen(8080, err => {
 	if(!err) console.log('Server started http://0.0.0.0:8080');
