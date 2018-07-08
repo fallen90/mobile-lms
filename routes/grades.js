74346993db403ee42b121e77cd67f42dcf7d46b1
Jasper John Cinatad <slytherin9090@gmail.com>
2018-07-08 20:44:48 +0800

    added add and update grade, added get grade by student, add delete grade

--- routes/grades.js
+++ routes/grades.js
@@ -1,19 +1,21 @@
 const api = require('express').Router();
 const { grades, students } = require('../lib/database');
 
-
+/** Update or Add Grades **/
 api.post('/students/:id/grades', (req, res) => {
     const student_id = req.params.id;
     const _ifExists = students.getById(student_id).size().value();
 
     if (_ifExists) {
     	const data = Object.assign(req.body || {}, { student_id });
+    	//check if grades already exists
+    	const _ifGradeExists = grades.find({ student_id }).size().value();
 		//add grades to user
-		const result = grades.push(data).write();
+		const grades_data = (_ifGradeExists) ? grades.findOne({ student_id }).assign(Object.assign(data, { modified_at : (new Date).toISOString() })).write() : grades.insert(Object.assign(data, { created_at : (new Date).toISOString() })).write();
 		return res.status(201).json({
     		status : 'success',
     		message : 'grades posted',
-    		data
+    		data : grades_data
     	});
     } else {
     	return res.status(400).json({
@@ -23,5 +25,46 @@ api.post('/students/:id/grades', (req, res) => {
     }
 });
 
+/** get Grade by student_id **/
+api.get('/students/:id/grades', (req, res) => {
+    const student_id = req.params.id;
+    const _ifExists = students.getById(student_id).size().value();
+
+    if (_ifExists) {
+    	const grades_data = grades.findOne({ student_id }).value();
+    	return res.status(200).json(Object.assign(grades_data));
+    } else {
+    	return res.status(400).json({
+    		status : 'error',
+    		message : 'student not found'
+    	});
+    }
+});
+
+/** clear grades **/
+api.delete('/students/:id/grades', (req, res) => {
+	const student_id = req.params.id;
+    const _ifExists = students.getById(student_id).size().value();
+
+    if (_ifExists) {
+    	grades.remove({ student_id }).write();
+    	return res.status(200).json({
+    		status : 'success',
+    		message : 'grade removed'
+    	});
+    } else {
+    	return res.status(400).json({
+    		status : 'error',
+    		message : 'student not found'
+    	});
+    }
+});
 
 module.exports = api;
+
+
+
+
+
+
+
